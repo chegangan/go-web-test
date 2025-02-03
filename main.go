63f365661f0f1241d1c8dd5a6d1d6291c11e05ff
chegan <2048684189@qq.com>
2025-02-03 11:58:19 +0800

    使用endless库 和http的shuntdown库完成了优雅的热重启（开启子进程代替父进程）和优雅的关闭（在超时时间内结束了所有连接才关闭），修改在main函数上

--- main.go
+++ main.go
@@ -1,25 +1,50 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"go-web-test/pkg/setting"
 	"go-web-test/routers"
-	"net/http"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/fvbock/endless"
 )
 
 func main() {
-	router := routers.InitRouter()
+	// endless实现优雅的热重启
+	endless.DefaultReadTimeOut = setting.ReadTimeout
+	endless.DefaultWriteTimeOut = setting.WriteTimeout
+	endless.DefaultMaxHeaderBytes = 1 << 20
+	endPoint := fmt.Sprintf(":%d", setting.HTTPPort)
 
-	s := &http.Server{
-		Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
-		Handler:        router,
-		ReadTimeout:    setting.ReadTimeout,
-		WriteTimeout:   setting.WriteTimeout,
-		MaxHeaderBytes: 1 << 20,
+	server := endless.NewServer(endPoint, routers.InitRouter())
+	server.BeforeBegin = func(add string) {
+		log.Printf("Actual pid is %d", syscall.Getpid())
 	}
 
-	err := s.ListenAndServe()
-	if err != nil {
-		return
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Printf("Server err: %v", err)
+		}
+	}()
+
+	// 等待中断信号以优雅地关闭服务器（设置 5 秒的超时时间）
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+
+	log.Println("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
 	}
+
+	log.Println("Server exiting")
 }
